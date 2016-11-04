6de4ff5c3de42fa42639ff070f2e40ea61245087
E-David <ericdavid365@gmail.com>
2016-11-04 12:18:25 -0500

    Add default route, add State and sync state

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,42 +3,35 @@ import ReactDOM from 'react-dom'
 import Backbone from 'backbone'
 import TweetView from './views/tweetview'
 
-const app = function() {
+console.log("helo")
+var app = function() {
 
 
 var TweetCollection = Backbone.Collection.extend({
-		url: "https://twitclone-example.now.sh/api/tweet",
-		parse: function(rawResponse){
-			
-			return rawResponse
-		}
+		url: "https://twitclone-example.now.sh/api/tweet"
 	})
 
 
 var Controller = Backbone.Router.extend({
 		routes: {
-			'home': 'handleHome',
+			"home": "handleHome",
+			"default": "handleDefault"
 		},
 
 		handleHome: function(){
 			var tweetCollection = new TweetCollection()
-			var promise = tweetCollection.fetch({})
+			tweetCollection.fetch()
 			console.log(tweetCollection)
-			promise.then(
-				function(){
-					ReactDOM.render(<TweetView collection={tweetCollection} />, document.querySelector('.container'))
-			})
-
-
+			
+			ReactDOM.render(<TweetView collection={tweetCollection} />, document.querySelector('.container'))
 		},
-		
+		handleDefault: function() {
+			location.hash = "home"
+		},
 		initialize: function(){
 			Backbone.history.start()
 		}
-
 	})
 	var controller = new Controller();
 }
-
-
 app()
